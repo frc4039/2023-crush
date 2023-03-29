bd4beccc4cd08cbc7294225a51a5af98631d4734
Blake <blakemcfadden05@gmail.com>
2023-03-29 18:40:58 -0400

    switched controller

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,6 +8,8 @@ import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -19,13 +21,13 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
+
+
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
-
+      
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
@@ -48,7 +50,7 @@ public class RobotContainer {
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+ 
   }
 
   /**
--- src/main/java/frc/robot/subsystems/Drivebase.java
+++ src/main/java/frc/robot/subsystems/Drivebase.java
@@ -11,31 +11,37 @@ import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMax.*;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 import frc.robot.Constants.*;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 public class Drivebase extends SubsystemBase {
     private DifferentialDrive m_crush;
-    private Joystick m_leftStick;
-    private Joystick m_rightStick;
-    private VictorSPX m_leftDriveMotor;
-    private VictorSPX m_rightDriveMotor;
+    private XboxController driver;
+   // private final int leftStick;
+    //private final int rightStick;
+    private WPI_VictorSPX m_leftDriveMotor;
+    private WPI_VictorSPX m_rightDriveMotor;
 
   /** Creates a new Subsystem. */
   public Drivebase() {
-    m_leftDriveMotor = new VictorSPX(DrivebaseConstants.kLeftDriveMotorID);
-    m_rightDriveMotor = new VictorSPX(DrivebaseConstants.kRightDriveMotorID);
+    m_leftDriveMotor = new WPI_VictorSPX(DrivebaseConstants.kLeftDriveMotorID);
+    m_rightDriveMotor = new WPI_VictorSPX(DrivebaseConstants.kRightDriveMotorID);
 
     m_leftDriveMotor.configFactoryDefault();
     m_rightDriveMotor.configFactoryDefault();
 
+    driver = new XboxController(DrivebaseConstants.kDriverControllerPort);
+
+    //leftStick = XboxController.Axis.kLeftY.value;
+    //rightStick = XboxController.Axis.kRightY.value;
+
     m_crush = new DifferentialDrive(m_leftDriveMotor, m_rightDriveMotor);
 
-    m_leftStick = new Joystick(DrivebaseConstants.kLeftJoystickPort);
-    m_rightStick = new Joystick(DrivebaseConstants.kRightJoystickPort);
-  }
+    }
 
   /**
    * Example command factory method.
@@ -64,7 +70,7 @@ public class Drivebase extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    m_crush.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+    m_crush.tankDrive(driver.getLeftY(), driver.getRightY());
   }
 
   @Override
