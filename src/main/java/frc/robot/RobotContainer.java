ac5d3b3f56c5bf8f24c4c4aeb36755d4378b3369
Pablo Cafiso <pablo.cafiso@verticalmotion.ca>
2023-03-31 16:05:37 -0400

    Removed unused import

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,7 +7,6 @@ package frc.robot;
 import frc.robot.Constants.DriverConstants;
 import frc.robot.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 /**
